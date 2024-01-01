020088c7bc0559d34a2fcbe5bac035de5781a82a
jeremyholbrecht <jerexyz@protonmail.com>
2024-01-01 12:45:21 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jeremyholbrecht/go-leetcode
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	problem0001 "github.com/jeremyholbrecht/go-leetcode/problems/0001.two-sum"
+)
+
+func main() {
+	fmt.Println(problem0001.TwoSum([]int{2, 7, 11, 15}, 9))
+}
