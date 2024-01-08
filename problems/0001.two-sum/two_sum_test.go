5fe3025c5a3068352f0388c4380daf987896e489
jeremyholbrecht <Griffley@proton.me>
2024-01-09 00:55:57 +0100

    solved two sum

--- problems/0001.two-sum/two_sum_test.go
+++ problems/0001.two-sum/two_sum_test.go
@@ -7,11 +7,32 @@ import (
 
 func TestTwoSum(t *testing.T) {
 	t.Run("Example 1", func(t *testing.T) {
-		got := []int{1, 2, 3}
-		want := []int{5, 5, 7}
+		got := TwoSum([]int{2, 7, 11, 15}, 9)
+		want := []int{0, 1}
 
-		if slices.Equal(got, want) {
-			t.Errorf("got %v want %v", got, want)
-		}
+		assertCorrectValue(t, got, want)
 	})
+
+	t.Run("Example 2", func(t *testing.T) {
+		got := TwoSum([]int{3, 2, 4}, 6)
+		want := []int{1, 2}
+
+		assertCorrectValue(t, got, want)
+	})
+
+	t.Run("Example 3", func(t *testing.T) {
+		got := TwoSum([]int{3, 3}, 6)
+		want := []int{0, 1}
+
+		assertCorrectValue(t, got, want)
+
+	})
+
+}
+
+func assertCorrectValue(t testing.TB, got, want []int) {
+	t.Helper()
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
