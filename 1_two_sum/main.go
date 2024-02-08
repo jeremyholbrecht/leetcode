f2180e273d95de893ea89d6b6a2de2612e1b8406
jeremyholbrecht <Griffley@proton.me>
2024-02-08 11:28:14 +0100

    working on solution with map

--- 1_two_sum/main.go
+++ 1_two_sum/main.go
@@ -1,5 +1,9 @@
 package main
 
+import "fmt"
+
+// brute force
+/*
 func twoSum(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		// setting j to i + 1 makes it so that the same elemen't won't be used twice
@@ -12,7 +16,25 @@ func twoSum(nums []int, target int) []int {
 	}
 	return []int{}
 }
+*/
 
+// map
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		// key = number, value = index of number
+		m[nums[i]] = i
+	}
+
+	// check if key is present in map
+	val, ok := m[7]
+	if ok {
+		fmt.Println(val)
+	}
+
+	return []int{}
+}
 func main() {
 	twoSum([]int{2, 7, 11, 15}, 9)
 }
